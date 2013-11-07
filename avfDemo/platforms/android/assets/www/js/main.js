67071c3bd88e4964acd2f5506033053f5e8477ad
p32424 <faeryqueen21@fullsail.edu>
2013-11-07 00:34:35 -0800

    Instagram search api working

--- avfDemo/platforms/android/assets/www/js/main.js
+++ avfDemo/platforms/android/assets/www/js/main.js
@@ -3,34 +3,31 @@ Author: Ariana Antonio
 Project 1
 AVF 1311
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+$('#search').on('pageinit', function(){
+	//var search = $('#searchBtn');
+    //search.addEventListener("click", searchApp);
+    //$('#searchBtn').click(function(){
+        //searchApp();
+    //});
+    $("#instaData").empty();
+    var screenOutput = function(info) {
+        
+        console.log(info);
+        //$("#instaData").html("<h2>Photos</h2>");
+        $.each(info.data, function(index, photo) {
+            var pic = "<div><img src='" + photo.images.thumbnail.url + "' class='picSize'/><p>'" + photo.caption.text + "'</p></div>";
+            $("#instaData").append(pic);
+        });
+    };
+    $("#submit").click(function(e) {
+        e.preventDefault();
+        var searchTerm = $("#searchInput").val();       
+        var tag = searchTerm
+        var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=b82b3459d471430d9d6955668c68d708";
+        $.getJSON(url, screenOutput);
+        console.log(url);
+        
+    });
+    
+});	
